68f385ce2f41eb729b22800bf8e0511748bf1a3c
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-20 00:12:31 +0530

    Configured Tailwind CSS and improved homepage UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 function App() {
   return (
-    <div>
-      <h1>StudentPG Bhopal</h1>
-      <p>Find your perfect PG in Bhopal</p>
-    </div>
-  );
+  <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <h1 className="text-5xl font-bold text-blue-600 mb-4">
+      StudentPG Bhopal
+    </h1>
+
+    <p className="text-xl text-gray-700">
+      Find your perfect PG in Bhopal
+    </p>
+  </div>
+);
 }
 
 export default App;
