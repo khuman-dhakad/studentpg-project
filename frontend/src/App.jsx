7e5fa48e543b1e381bf3e45b772583e176a0650a
Khuman Dhakad <khuman.dhakad3822@gmail.com>
2026-06-20 00:49:49 +0530

    Created StudentPG navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar";
 function App() {
   return (
   <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+     <Navbar />
     <h1 className="text-5xl font-bold text-blue-600 mb-4">
       StudentPG Bhopal
     </h1>
-
     <p className="text-xl text-gray-700">
       Find your perfect PG in Bhopal
     </p>
